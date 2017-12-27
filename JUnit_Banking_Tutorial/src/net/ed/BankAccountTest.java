cd2e2cd5df9a17a4956188f70344d6871af0927a
Edward Dupre <edwd42@gmail.com>
2017-12-27 11:07:15 -0500

    implement deposit test

--- JUnit_Banking_Tutorial/src/net/ed/BankAccountTest.java
+++ JUnit_Banking_Tutorial/src/net/ed/BankAccountTest.java
@@ -6,17 +6,20 @@ import static junit.framework.TestCase.fail;
 public class BankAccountTest {
 
     @org.junit.Test
-    public void getBalance() {
-        fail("This test not yet implemented");
-    }
-
-    @org.junit.Test
     public void deposit() {
-        fail("This test not yet implemented");
+//        fail("This test not yet implemented");
+        BankAccount account = new BankAccount("Me", "LaStName", 1000);
+        double balance = account.deposit(200, true);
+        assertEquals(1200, balance, 0);
     }
 
     @org.junit.Test
     public void withdraw() {
+        fail("This test not yet implemented");
+    }
+
+    @org.junit.Test
+    public void getBalance() {
         fail("This test not yet implemented");
     }
 }
