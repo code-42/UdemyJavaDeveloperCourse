505109ed6e56fe6cca002ad3cc7dbf6e45db5731
Edward Dupre <edwd42@gmail.com>
2017-12-27 10:07:34 -0500

    add tests and junit.framework

---
+++ JUnit_Banking_Tutorial/src/net/ed/BankAccountTest.java
@@ -0,0 +1,22 @@
+package net.ed;
+
+import static org.junit.Assert.*;
+import static junit.framework.TestCase.fail;
+
+public class BankAccountTest {
+
+    @org.junit.Test
+    public void getBalance() {
+        fail("This test not yet implemented");
+    }
+
+    @org.junit.Test
+    public void deposit() {
+        fail("This test not yet implemented");
+    }
+
+    @org.junit.Test
+    public void withdraw() {
+        fail("This test not yet implemented");
+    }
+}
--- JUnit_Banking_Tutorial/src/net/ed/Main.java
+++ JUnit_Banking_Tutorial/src/net/ed/Main.java
@@ -3,6 +3,6 @@ package net.ed;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("The application is running");
     }
 }
