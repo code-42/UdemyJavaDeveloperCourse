f94abedafcdf6f503ab2724c6d36049f8c21ee94
Edward Dupre <edwd42@gmail.com>
2017-12-27 11:21:11 -0500

    add test for accountType and assertTrue() method

--- JUnit_Banking_Tutorial/src/net/ed/BankAccountTest.java
+++ JUnit_Banking_Tutorial/src/net/ed/BankAccountTest.java
@@ -8,7 +8,7 @@ public class BankAccountTest {
     @org.junit.Test
     public void deposit() {
 //        fail("This test not yet implemented");
-        BankAccount account = new BankAccount("Me", "LaStName", 1000);
+        BankAccount account = new BankAccount("Me", "LaStName", 1000, BankAccount.CHECKING);
         double balance = account.deposit(200, true);
         assertEquals(1200, balance, 0);
     }
@@ -20,7 +20,7 @@ public class BankAccountTest {
 
     @org.junit.Test
     public void getBalance_after_deposit() {
-        BankAccount account = new BankAccount("Me", "LaStName", 1000);
+        BankAccount account = new BankAccount("Me", "LaStName", 1000, BankAccount.CHECKING);
         account.deposit(200, true);
         assertEquals(1200, account.getBalance(), 0);
 
@@ -28,8 +28,14 @@ public class BankAccountTest {
 
     @org.junit.Test
     public void getBalance_after_withdraw() {
-        BankAccount account = new BankAccount("Me", "LaStName", 1000);
+        BankAccount account = new BankAccount("Me", "LaStName", 1000, BankAccount.CHECKING);
         account.withdraw(200, true);
         assertEquals(800, account.getBalance(), 0);
+    }
+
+    @org.junit.Test
+    public void isChecking_true(){
+        BankAccount account = new BankAccount("Me", "LaStName", 1000, BankAccount.SAVINGS);
+        assertTrue(account.isChecking());
     }
 }
