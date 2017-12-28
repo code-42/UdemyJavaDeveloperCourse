abf964060dad0dc81896b45cb5b69e3d3c5100cd
Edward Dupre <edwd42@gmail.com>
2017-12-27 19:40:33 -0500

    modify Utilities Test class to create instance of Utilities class in setup()

--- JUnit_Challenge_1/src/net/ed/UtilitiesTest.java
+++ JUnit_Challenge_1/src/net/ed/UtilitiesTest.java
@@ -4,19 +4,26 @@ import static org.junit.Assert.*;
 
 public class UtilitiesTest {
 
+    private Utilities util;
+
+    @org.junit.Before
+    public void setup(){
+        util = new Utilities();
+    }
+
     @org.junit.Test
     public void everyNthChar() {
-        Utilities utils = new Utilities();
+//        Utilities utils = new Utilities();
 //        char[] output = utils.everyNthChar(new char[] {'h', 'e', 'l', 'l', 'o'},2);
 //        assertArrayEquals(new char[] {'e','l'},output);
         char[] charArr = {'h', 'e', 'l', 'l', 'o'};
-        char[] result = utils.everyNthChar(charArr,8);
+        char[] result = util.everyNthChar(charArr,2);
         assertArrayEquals(new char[] {'e', 'l'}, result);
     }
 
     @org.junit.Test
     public void removePairs() {
-        Utilities util = new Utilities();
+//        Utilities util = new Utilities();
 //        assertEquals("ABCDEF", util.removePairs("AABCDDEFF"));
 //        assertEquals("ABCABDEF", util.removePairs("ABCCABDEEF"));
 //        assertNull("String is null", util.removePairs(null));
@@ -26,7 +33,7 @@ public class UtilitiesTest {
     @org.junit.Test
     public void converter() {
         // return (a/b) + (a * 30) -2;
-        Utilities util = new Utilities();
+//        Utilities util = new Utilities();
         int sum = util.converter(10,5);
         assertEquals(300,sum);
     }
@@ -34,13 +41,13 @@ public class UtilitiesTest {
     // Challenge #8 ArithmeticException (divide by 0)
     @org.junit.Test(expected = ArithmeticException.class)
     public void converter_arithmeticException() throws Exception {
-        Utilities util = new Utilities();
+//        Utilities util = new Utilities();
         util.converter(10,0);
     }
 
     @org.junit.Test
     public void nullIfOddLength() {
-        Utilities util = new Utilities();
+//        Utilities util = new Utilities();
         assertNull(util.nullIfOddLength("odd"));
         assertNotNull(util.nullIfOddLength("even"));
     }
