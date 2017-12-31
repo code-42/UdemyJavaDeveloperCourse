50460ca4b1d37aec440f15c542dc49f3dd7cf6a2
Edward Dupre <edwd42@gmail.com>
2017-12-30 22:01:32 -0500

    add class Player with constructor and getters and setters

---
+++ Interfaces/src/net/ed/Player.java
@@ -0,0 +1,47 @@
+package net.ed;
+
+public class Player {
+
+    private String name;
+    private int hitPoints;
+    private int strength;
+    private String weapon;
+
+    public Player(String name, int hitPoints, int strength) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
+}
