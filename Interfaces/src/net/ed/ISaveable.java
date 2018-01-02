26fe2fe37df47b6185223b90ef1ef700c7075425
Edward Dupre <edwd42@gmail.com>
2018-01-01 23:01:51 -0500

    add ISaveable interface and implement methods in Player class

---
+++ Interfaces/src/net/ed/ISaveable.java
@@ -0,0 +1,8 @@
+package net.ed;
+
+import java.util.List;
+
+public interface ISaveable {
+    List<String> write();
+    void read(List<String> savedValues);
+}
--- Interfaces/src/net/ed/Player.java
+++ Interfaces/src/net/ed/Player.java
@@ -1,6 +1,9 @@
 package net.ed;
 
-public class Player {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player implements ISaveable {
 
     private String name;
     private int hitPoints;
@@ -43,5 +46,37 @@ public class Player {
 
     public void setWeapon(String weapon) {
         this.weapon = weapon;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", hitPoints=" + hitPoints +
+                ", strength=" + strength +
+                ", weapon='" + weapon + '\'' +
+                '}';
+    }
+
+    @Override
+    public List<String> write() {
+        List<String> values = new ArrayList<String>();
+        values.add(0, this.name);
+        values.add(1, "" + this.hitPoints);
+        values.add(2, "" + this.strength);
+        values.add(3, this.weapon);
+
+        return values;
+    }
+
+    @Override
+    public void read(List<String> savedValues) {
+        if (savedValues != null && savedValues.size() > 0) {
+            this.name = savedValues.get(0);
+            this.hitPoints = Integer.parseInt(savedValues.get(1));
+            this.strength = Integer.parseInt(savedValues.get(2));
+            this.weapon = savedValues.get(3);
+        }
+
     }
 }
