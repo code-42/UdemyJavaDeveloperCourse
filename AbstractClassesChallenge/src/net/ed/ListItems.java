88916a0b4e54e74beb2181e46fd55e9d9d368707
Edward Dupre <edwd42@gmail.com>
2018-01-05 20:37:13 -0500

    add JavaDoc to Test class

--- AbstractClassesChallenge/src/net/ed/ListItems.java
+++ AbstractClassesChallenge/src/net/ed/ListItems.java
@@ -1,10 +1,20 @@
 package net.ed;
 
+/**
+ * Creates a new ListItem
+ *
+ */
 public abstract class ListItems {
 
     private ListItems nextItem = null;
     private ListItems prevItem = null;
 
+    /**
+     * Moves to the next ListItem
+     * Moves back to the previous ListItem
+     *
+     * @return
+     */
     abstract ListItems next();
     abstract ListItems previous();
 
--- AbstractClassesChallenge/src/net/ed/ListItemsTest.java
+++ AbstractClassesChallenge/src/net/ed/ListItemsTest.java
@@ -4,11 +4,19 @@ import static org.junit.Assert.*;
 
 public class ListItemsTest {
 
+    /**
+     * Test of next() method of ListItems class
+     * @throws Exception
+     */
     @org.junit.Test
     public void next() throws Exception {
         fail("This test not yet implemented");
     }
 
+    /**
+     * Test of previous() method of ListItems class
+     * @throws Exception
+     */
     @org.junit.Test
     public void previous() throws Exception {
         fail("This test not yet implemented");
