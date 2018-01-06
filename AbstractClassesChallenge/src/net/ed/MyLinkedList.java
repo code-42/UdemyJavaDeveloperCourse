9dfe4ef2f09b4f24f7baf7092c7f8a6d5659b432
Edward Dupre <edwd42@gmail.com>
2018-01-06 12:37:36 -0500

    implemented removeItem() and traverse() methods in MyLinkedList class

--- AbstractClassesChallenge/src/net/ed/MyLinkedList.java
+++ AbstractClassesChallenge/src/net/ed/MyLinkedList.java
@@ -61,6 +61,35 @@ public class MyLinkedList implements NodeList {
 
     @Override
     public boolean removeItem(ListItem item) {
+        if(item != null) {
+            System.out.println("Deleting item " + item.getValue());
+        }
+
+        ListItem currentItem = this.root;
+        while(currentItem != null) {
+            int comparison = currentItem.compareTo(item);
+            if(comparison == 0) {
+                // found the item to delete
+                if(currentItem == this.root) {
+                    this.root = currentItem.next();
+                } else {
+                    currentItem.previous().setNext(currentItem.next());
+                    if(currentItem.next() != null) {
+                        currentItem.next().setPrevious(currentItem.previous());
+                    }
+                }
+                return true;
+            } else if(comparison <0) {
+                currentItem = currentItem.next();
+            } else { // comparison > 0
+                // We are at an item greater than the one to be deleted
+                // so the item is not in the list
+                return false;
+            }
+        }
+
+        // We have reached the end of the list
+        // Without finding the item to delete
         return false;
     }
 
@@ -71,7 +100,15 @@ public class MyLinkedList implements NodeList {
      */
     @Override
     public void traverse(ListItem root) {
-        //
+        if(root == null) {
+            System.out.println("The list is empty");
+        } else {
+            while (root != null) {
+                System.out.println(root.getValue());
+                root = root.next();
+            }
+        }
+
 
     }
 }
